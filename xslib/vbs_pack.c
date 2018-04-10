halftwo/knotty

commit ebd4edb2de0b6db4b3116132e94bd1a5a8a3b190

function _find_first_bit_set() now return 0 for the LSB bit and 63 for the MSB bit

xslib/vbs_pack.c
@@ -117,7 +117,7 @@ static inline size_t _tag_size(uintmax_t num)
 	return n;
 }
 
-/* The MSB is position 64 (or 32), the LSB is position 1. */
+/* The MSB is position 63 (or 31), the LSB is position 0. */
 static inline int _find_last_bit_set(intmax_t v)
 {
 	int r = sizeof(intmax_t) * 8;
@@ -125,7 +125,7 @@ static inline int _find_last_bit_set(intmax_t v)
 	if (v == 0)
 		return 0;
 
-	while (v > 0)
+	while (v != 0)
 	{
 		v <<= 1;
 		--r;
@@ -208,8 +208,8 @@ int vbs_make_double_value(double* value, intmax_t significand, int expo)
 			significand = -significand;
 		}
 
-		point = _find_last_bit_set(significand) - 1;
-		expo = expo + IEEE754_BINARY64_BIAS + point;
+		point = _find_last_bit_set(significand);
+		expo += IEEE754_BINARY64_BIAS + point;
 
 		if (expo >= 0x7ff)
 		{

commit 6e53d7d03cb62a865c6bb7dbc2647035400e6eda

Fixed the bug in function _find_last_bit_set() introduced in commit ebd4edb2de0b6db4b3116132e94bd1a5a8a3b190

xslib/Makefile
@@ -2,7 +2,7 @@
 ##
 
 EDITION  = 180325
-REVISION = 180327
+REVISION = 180410
 RELEASE  = 20
 
 XSLIB_VERSION = $(EDITION).$(REVISION).$(RELEASE)

xslib/vbs_pack.c
@@ -118,17 +118,13 @@ static inline size_t _tag_size(uintmax_t num)
 }
 
 /* The MSB is position 63 (or 31), the LSB is position 0. */
-static inline int _find_last_bit_set(intmax_t v)
+static inline int _find_last_bit_set(uintmax_t v)
 {
-	int r = sizeof(intmax_t) * 8;
-
-	if (v == 0)
-		return 0;
-
+	int r = -1;
 	while (v != 0)
 	{
-		v <<= 1;
-		--r;
+		v >>= 1;
+		++r;
 	}
 
 	return r;